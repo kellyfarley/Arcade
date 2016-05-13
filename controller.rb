f67858aceb4e52850a92f77c7884e9e1525554e0
Kelly Farley <farleykc@gmail.com>
2016-05-13 11:59:47 -0500

    final

--- controller.rb
+++ controller.rb
@@ -1,16 +1,40 @@
 require 'sinatra'
 require_relative 'models/rps.rb'
+enable :sessions
+
+get '/play' do
+	name = session[:name]
+	# erb :index
+	
+	# p1 = Player.new
+	# p1.name = (params[:name])
+	# p1.computer_choice
+
+	# game = Game.new
+	# game.game_time
+
+	# @results = game.game_time
+
+	# erb :results
+end
+
+get '/name/:name' do
+	session[:name] = params[:name]
+	redirect '/play'
+end
 
 get '/' do
 	erb :index
 end
 
 post '/' do
-	p1 = Player.new
-	p1.name = (params[:name])
-	p1.computer_choice
+	p1 = Player.new(params[:name])
 	
-	game = Game.new
+	# p1.computer_choice = ["rock", "paper", "scissors"].shuffle
+	
+	game = Game.new(p1, computer)
+	p1.response = params[:weapon]
+
 	game.game_time
 
 	@results = game.game_time
